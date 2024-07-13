b93edc2275dfe166c89105f528fb7c7387ca6d63
ESWAPANTHI <swapanthi.eduri@gmail.com>
2024-07-13 13:46:11 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://mern-backend-1-lg1g.onrender.com' });
 
 export const login = (userData) => API.post('/auth/login', userData);
 export const signup = (userData) => API.post('/auth/signup', userData);
