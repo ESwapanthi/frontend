17b79e8326af66d5248845d84734c5e87511159b
ESwapanthi <130217394+ESwapanthi@users.noreply.github.com>
2024-07-13 14:42:12 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://mern-backend-1-lg1g.onrender.com' });
+const API = axios.create({ baseURL: 'https://backend-dn2k.onrender.com/api' });
 
 export const login = (userData) => API.post('/auth/login', userData);
 export const signup = (userData) => API.post('/auth/signup', userData);
