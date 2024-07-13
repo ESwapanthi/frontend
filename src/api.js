8186988d0a76ea335f8acf95c2bb34bdb5fec2b3
ESwapanthi <130217394+ESwapanthi@users.noreply.github.com>
2024-07-13 14:51:16 +0530

    Delete src/api.js

--- src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'https://backend-dn2k.onrender.com/api' });
-
-export const login = (userData) => API.post('/auth/login', userData);
-export const signup = (userData) => API.post('/auth/signup', userData);
